b813fcccb7d9e686fcfe06098d3f7b249fe9b088
misaaldsouza <52240946+misaaldsouza@users.noreply.github.com>
2022-03-28 17:07:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"misaal.dsouza@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
